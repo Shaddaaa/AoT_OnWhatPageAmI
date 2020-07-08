3467bb32e8bd4f51653cde57771380cf2c8c9663
Shaddaaa <florian@kaufmann.cloud>
2020-07-08 18:18:09 +0200

    combined old and new methods as the new method didn't work that great for /mods/modid changed formatting for profiles

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let ShaddasOnWhatPageAmI = {};
-ShaddasOnWhatPageAmI.text = window.location.href.replace("https://ageoftrades.com/game/", "");
-ShaddasOnWhatPageAmI.text = ShaddasOnWhatPageAmI.text.charAt(0).toUpperCase() + ShaddasOnWhatPageAmI.text.slice(1);
-document.title += " - " + ShaddasOnWhatPageAmI.text;
+ShaddasOnWhatPageAmI.text = window.location.href.replace("https://ageoftrades.com/game", "");
+ShaddasOnWhatPageAmI.text = ShaddasOnWhatPageAmI.text.replaceAll("/", " - ");
+if (ShaddasOnWhatPageAmI.text.indexOf("mods - ")!=-1) {
+	ShaddasOnWhatPageAmI.text = " - " + document.getElementById("main-").children[0].innerHTML;
+} else {
+	//make the first letter uppercase
+	ShaddasOnWhatPageAmI.text = ShaddasOnWhatPageAmI.text.slice(0,3) + ShaddasOnWhatPageAmI.text.charAt(3).toUpperCase() + ShaddasOnWhatPageAmI.text.slice(4);
+}
+
+document.title += ShaddasOnWhatPageAmI.text;
